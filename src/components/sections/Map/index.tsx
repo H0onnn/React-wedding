e06ce718774f7110fc40cb8ddcd52f9040ff3274
H0onnn <dukei2017@naver.com>
2024-02-22 17:15:12 +0900

    Feat: Map

---
+++ src/components/sections/Map/index.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useRef } from 'react';
+import { Location } from '@/models/wedding';
+import classNames from 'classnames/bind';
+import styles from './Map.module.scss';
+import Section from '@shared/Section';
+
+const cx = classNames.bind(styles);
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+interface Props {
+  location: Location;
+}
+
+const Map = ({ location }: Props) => {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAP_API_KEY}&autoload=false`;
+    script.async = true;
+
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const position = new window.kakao.maps.LatLng(
+          location.lat,
+          location.lng,
+        );
+
+        const options = {
+          center: position,
+          level: 3,
+        };
+
+        const marker = new window.kakao.maps.Marker({
+          position,
+        });
+
+        const map = new window.kakao.maps.Map(mapContainer.current, options);
+        marker.setMap(map);
+      });
+    };
+  }, []);
+
+  return (
+    <Section
+      title={
+        <div className={cx('wrap-header')}>
+          <span className={cx('txt-title')}>오시는 길</span>
+          <span className={cx('txt-subtitle')}>{location.name}</span>
+          <span className={cx('txt-subtitle')}>{location.address}</span>
+        </div>
+      }
+    >
+      <div className={cx('wrap-map')}>
+        <div className={cx('map')} ref={mapContainer}></div>
+        <a
+          className={cx('btn-find-way')}
+          href={location.link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          길찾기
+        </a>
+      </div>
+
+      <div>
+        <WayToCome label="버스" list={location.waytocome.bus} />
+        <WayToCome label="지하철" list={location.waytocome.metro} />
+      </div>
+    </Section>
+  );
+};
+
+export default Map;
+
+interface WayToComeProps {
+  label: React.ReactNode;
+  list: string[];
+}
+
+const WayToCome = ({ label, list }: WayToComeProps) => {
+  return (
+    <div className={cx('wrap-waytocome')}>
+      <div className={cx('txt-label')}>{label}</div>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
